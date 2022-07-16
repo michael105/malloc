fa3ea7fdd9c8d5861bcc2a25e8fdc61f0f9af010
Michael Myer <misc.myer@zoho.com>
2022-07-16 02:34:26 +0000

    addmem

---
+++ addmem.c
@@ -0,0 +1,17 @@
+//+doc move cbrk (malloc_brk internal)
+//+def 
+MF int ml_malloc_brk_addmem(void* brkmin){
+		// calculate memory to preallocate
+		preallocate <<=1;
+		// grow progressively
+		preallocate += ((preallocate>>BRK_PROGFACTOR)<<BRK_PROGFACTOR);
+		// max is 1<<27 == 132MB
+		preallocate = ((preallocate-1) & ((1<<27)-1) )+1;
+		// round up to the next page 
+		if ( brk( (void*)((POINTER)(brkmin + (preallocate +PAGESIZE-1) ) & (~(PAGESIZE-1))) ) < 0 ){
+			warn("NOMEM2\n");
+			return(0);
+		}
+		return(1);
+}
+
