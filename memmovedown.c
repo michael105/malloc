b989ed0def242ac81e9a95f88e88dc91ddcbe6a0
Michael Myer <misc.myer@zoho.com>
2022-07-16 03:17:24 +0000

    memmovedown

---
+++ memmovedown.c
@@ -0,0 +1,22 @@
+
+//+doc copy overlapping areas, from upper to lower.
+// (s>d)
+// This does vector copy operations of longs, 64bit with amd64
+//+def
+MF void memmovedown(void *d, void *s, ml_size_t size){
+#define ASSERT(exp) 
+	ASSERT(d<ULONG_MAX-sizeof(long));
+	ASSERT(d<s);
+
+	if ( d>=s )
+		ABORT;
+
+	void *e = d+size;
+
+	while ( d < e-sizeof(long) )
+		*(long*)d++ = *(long*)s++;
+
+	while ( d<e )
+		*(char*)d++ = *(char*)s++;
+}
+
