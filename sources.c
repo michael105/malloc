b8f0564f13b9002a688c9318d3483ba838d89790
Michael Myer <misc.myer@zoho.com>
2022-07-19 22:32:14 +0000

    bk

--- sources.c
+++ sources.c
@@ -1,8 +1,26 @@
+ unsigned long preallocate;
+ unsigned long _cbrk;
+ unsigned long _brk;
+ unsigned long _brk_start;
+
+#define ASSERT(exp) 
+
+#include <stdio.h>
+#include <sys/mman.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <err.h>
+#include <errno.h>
+
+
+
 // combined sources
 #include "ml_freearray.c"
-#include "ml_malloc_brk_movcbrk.c"
-#include "memmovedown.c"
 #include "addmem.c"
+#include "memmovedown.c"
 #include "malloc.c"
 #include "free.c"
 #include "realloc.c"
